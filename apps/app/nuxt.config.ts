7f460224b56d4e666e22e163b974044b8e1b5b19
Kacy Luzzardi <kacy@kume.co>
2026-08-10 23:24:55 +0200

    Shadcn-vue and tailwindcss

--- apps/app/nuxt.config.ts
+++ apps/app/nuxt.config.ts
@@ -1,6 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
 export default defineNuxtConfig({
 	// Developement & Nuxt configuration
 	compatibilityDate: '2025-07-15',
+
 	devtools: { enabled: true },
+
+	// Assets
+	css: ['~/assets/style.css'],
+
+	// Vite
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	// Modules
+	modules: ['shadcn-nuxt'],
+	shadcn: {
+		prefix: '',
+	},
 });
